bb4259f435ddabd55d524d7395f4aa562f0a0ef2
Karol Arvizu <lorakarvizu@gmail.com>
2026-04-12 17:48:20 -0600

    detalles para el deploy

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import 'dotenv/config'
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
-export const prisma = new PrismaClient({ adapter })
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
